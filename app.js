5e83feb9d1b5206d71c694198caf734363077ea3
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2020-08-20 23:49:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express=require('express');
 const path= require('path');
 const uploadModel= require('./models/images');
 
+//for image manipulation
+const sharp= require('sharp');
+
 
 //for uploadind files
 const multer=require('multer');
 const ejs=require('ejs');
 
+//app
+const app=express();
+
+//set view enginge
+app.set('view engine','ejs');
+
+
+//public to static folder
+app.use(express.static('./public'));
+
 //store in uploads folder
 const store= multer.diskStorage({
-    destination:'./public/uploads',
+    destination:'./public/uploads/thumbs',
     filename: (req,file,callback)=>{
 
 //path module has extname included
@@ -17,7 +30,6 @@ const store= multer.diskStorage({
             Date.now() + path.extname( file.originalname))
 
     }
-
 });
 
 //fetch data from db
@@ -42,64 +54,53 @@ var image =multer({
     }
 }
 
-//app
-const app=express();
-
-//set view enginge
-app.set('view engine','ejs');
-
-
-//public to static folder
-app.use(express.static('./public'));
 
 app.get('/',(req,res)=>{
     res.render('main')
 });
 
-
 //post
 app.post('/uploads', (req,res)=>{
-    
-
     image(req,res,(error) =>{
         if(error){
             res.render('main',{
                 info: error
             });
-
         }else{
             if(req.file==undefined){
                 res.render('main',{
                     info: "you need to select a image!"
-                })
-            }else{
-                
-                var imgfile=req.file.filename;
-                var imgdetails= new uploadModel({
-                filename:imgfile,
                 });
-                imgdetails.save(function(error,doc){
-                    imgdata.exec(function(error,data){
-                        if(error){
-                            throw error;
-                        }else{
-                            res.render('pictures', {
-                                file:`uploads/${req.file.filename}`,
-                                data:data
-    
-                            });
-                        }
-                    })
-                   
+            }else{
+                var imgfile=sharp(req.file.path).resize(50, 50).toFile('uploads' + 'thumbs' + req.file.originalname, 
+                (err, resizeImage) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        //console.log(resizeImage);
+                        var imgdetails= new uploadModel({
+                        filename:imgfile,
+                        });
+                        imgdetails.save(function(error,doc){
+                            imgdata.exec(function(error,data){
+                                if(error){
+                                    throw error;
+                                }else{
+                                    res.render('pictures', {
+                                        file:`uploads/thumbs/${req.file.filename}`,
+                                        data:data
+                                    });
+                                }
+                            })
+                           
+                        })
+                    }
                 })
-                
-                
-                
-
             }
         }
     })
 });
+
 
 app.get('/uploads',(req,res)=>{
     imgdata.exec(function(error,data){
@@ -107,7 +108,7 @@ app.get('/uploads',(req,res)=>{
             throw error;
         }else{
             res.render('pictures', {
-                file:`uploads/${req.file.filename}`,
+                file:`uploads/thumbs/${req.file.filename}`,
                 data:data
 
             });
